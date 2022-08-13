78ec8c08b2311055e1c0cb1c70d73a09b152981e
lopezalexander <lopez-alexander@hotmail.com>
2022-08-12 22:25:54 -0400

    Simplifier les pages pour lister a 1 au lieu de 3, ajouter split-pane pour web view, ajouter icone menu

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,42 @@
 <template>
   <ion-app>
-      <ion-menu content-id="main-content" type="overlay">
+    <ion-split-pane content-id="main-content">
+      <ion-menu content-id="main-content" type="reveal">
         <ion-content>
           <ion-list>
             <ion-list-header>Mes recettes</ion-list-header>
             <ion-menu-toggle :auto-hide="false">
               <ion-item router-link="/home">
-                <ion-icon slot="start" :ios="logoTux" :md="logoTux"></ion-icon>
+                <ion-icon slot="start" :ios="storefront" :md="storefront"></ion-icon>
                 <ion-label>Accueil</ion-label>
               </ion-item>
-              <ion-item router-link="/ChickenList">
-                <ion-icon slot="start" :ios="logoTux" :md="logoTux"></ion-icon>
+              <ion-item router-link="/liste/chicken">
+                <ion-icon slot="start" :ios="logoTwitter" :md="logoTwitter"></ion-icon>
                 <ion-label>Chicken</ion-label>
               </ion-item>
-              <ion-item router-link="/SeafoodList">
-                <ion-icon slot="start" :ios="logoTux" :md="logoTux"></ion-icon>
+              <ion-item router-link="/liste/seafood">
+                <ion-icon slot="start" :ios="fish" :md="fish"></ion-icon>
                 <ion-label>Seafood</ion-label>
               </ion-item>
-              <ion-item router-link="/DessertList">
-                <ion-icon slot="start" :ios="logoTux" :md="logoTux"></ion-icon>
+              <ion-item router-link="/liste/dessert">
+                <ion-icon slot="start" :ios="iceCream" :md="iceCream"></ion-icon>
                 <ion-label>Dessert</ion-label>
               </ion-item>
             </ion-menu-toggle>
           </ion-list>
         </ion-content>
       </ion-menu>
-      <ion-router-outlet id="main-content">
-      </ion-router-outlet>
+      <ion-router-outlet id="main-content"> </ion-router-outlet>
+    </ion-split-pane>
   </ion-app>
 </template>
 
 <script lang="ts">
-import { 
-  IonApp,
-  IonRouterOutlet,
-  IonContent, 
-  IonList, 
-  IonListHeader,
-  IonItem,
-  IonLabel,
-  IonMenu,
-  IonMenuToggle,
-  IonIcon,
- } from '@ionic/vue';
-import { defineComponent } from 'vue';
-import { logoTux } from 'ionicons/icons';
+import { IonApp, IonRouterOutlet, IonContent, IonList, IonListHeader, IonItem, IonLabel, IonMenu, IonMenuToggle, IonIcon } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { storefront, logoTwitter, fish, iceCream } from "ionicons/icons";
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
     IonRouterOutlet,
@@ -59,10 +49,13 @@ export default defineComponent({
     IonMenuToggle,
     IonIcon,
   },
-  setup(){
+  setup() {
     return {
-      logoTux
-    }
-  }
+      storefront,
+      logoTwitter,
+      fish,
+      iceCream,
+    };
+  },
 });
 </script>
--- src/views/DetailsRecette.vue
+++ src/views/DetailsRecette.vue
@@ -3,37 +3,39 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-buttons slot="start">
-            <ion-back-button @click="this.router.back()"></ion-back-button>
-        </ion-buttons>  
+          <ion-back-button @click="this.router.back()"></ion-back-button>
+        </ion-buttons>
         <ion-title>Recette</ion-title>
-        <!-- <router-link to="/ChickenList/4">ChickenList</router-link><br>
-        <router-link to="/SeafoodList">SeafoodList</router-link><br>
-        <router-link to="/DessertList">DessertList</router-link> -->
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">    
-        <ion-item>
-            <ion-img :src="this.maListe[0].image"></ion-img>
-        </ion-item><br>
-        <ion-label id="meal-title">{{this.maListe[0].nom}}</ion-label><br><br>
-        <ion-label id="ion-label">Origine: {{this.maListe[0].origine}}</ion-label><br>
-        <ion-label id="ion-label">Catégorie: {{this.maListe[0].categorie}}</ion-label><br><br>
-        <ion-label id="title2">Ingrédients</ion-label><br>
-        <ul id="ingredientsList" v-for="item in this.maListe[0].ingredients" :key="item">
-            <li v-if="item.length>1"><small>{{item}}</small></li>
-        </ul>
+
+    <ion-content :fullscreen="true">
+      <ion-item>
+        <ion-img :src="this.maListe[0].image"></ion-img> </ion-item
+      ><br />
+      <ion-label id="meal-title">{{ this.maListe[0].nom }}</ion-label
+      ><br /><br />
+      <ion-label id="ion-label">Origine: {{ this.maListe[0].origine }}</ion-label
+      ><br />
+      <ion-label id="ion-label">Catégorie: {{ this.maListe[0].categorie }}</ion-label
+      ><br /><br />
+      <ion-label id="title2">Ingrédients</ion-label><br />
+      <ul id="ingredientsList" v-for="item in this.maListe[0].ingredients" :key="item">
+        <li v-if="item.length > 1">
+          <small>{{ item }}</small>
+        </li>
+      </ul>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/vue';
-import { defineComponent } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from "@ionic/vue";
+import { defineComponent } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 export default defineComponent({
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     IonContent,
     IonHeader,
@@ -43,52 +45,71 @@ export default defineComponent({
     IonButtons,
     IonBackButton,
   },
-  setup(){
+  setup() {
     const route = useRoute();
     const router = useRouter();
     const id = route.params.id;
-    return {route,router,id};
+    return { route, router, id };
   },
-  data(){
+  data() {
     return {
-        maListe:[{}],
-    }
+      maListe: [{}],
+    };
   },
 
   ionViewDidEnter() {
     this.getJSON();
   },
 
-  methods:{
+  methods: {
     async getJSON() {
-        let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=`+this.id;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let temp: object[] = [];
-            temp.push({id: data.meals[0].idMeal, nom: data.meals[0].strMeal, origine: data.meals[0].strArea, 
-            categorie: data.meals[0].strCategory, image: data.meals[0].strMealThumb,
-            ingredients: [data.meals[0].strIngredient1, data.meals[0].strIngredient2, 
-            data.meals[0].strIngredient3, data.meals[0].strIngredient4, data.meals[0].strIngredient5, 
-            data.meals[0].strIngredient6, data.meals[0].strIngredient7, data.meals[0].strIngredient8, 
-            data.meals[0].strIngredient9, data.meals[0].strIngredient10, data.meals[0].strIngredient11, 
-            data.meals[0].strIngredient12, data.meals[0].strIngredient13, data.meals[0].strIngredient14, 
-            data.meals[0].strIngredient15, data.meals[0].strIngredient16, data.meals[0].strIngredient17, 
-            data.meals[0].strIngredient18, data.meals[0].strIngredient19, data.meals[0].strIngredient20] });
-            
-            this.maListe = temp;
+      let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + this.id;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          let temp: object[] = [];
+          temp.push({
+            id: data.meals[0].idMeal,
+            nom: data.meals[0].strMeal,
+            origine: data.meals[0].strArea,
+            categorie: data.meals[0].strCategory,
+            image: data.meals[0].strMealThumb,
+            ingredients: [
+              data.meals[0].strIngredient1,
+              data.meals[0].strIngredient2,
+              data.meals[0].strIngredient3,
+              data.meals[0].strIngredient4,
+              data.meals[0].strIngredient5,
+              data.meals[0].strIngredient6,
+              data.meals[0].strIngredient7,
+              data.meals[0].strIngredient8,
+              data.meals[0].strIngredient9,
+              data.meals[0].strIngredient10,
+              data.meals[0].strIngredient11,
+              data.meals[0].strIngredient12,
+              data.meals[0].strIngredient13,
+              data.meals[0].strIngredient14,
+              data.meals[0].strIngredient15,
+              data.meals[0].strIngredient16,
+              data.meals[0].strIngredient17,
+              data.meals[0].strIngredient18,
+              data.meals[0].strIngredient19,
+              data.meals[0].strIngredient20,
+            ],
+          });
 
-        })
-    }
-  }
+          this.maListe = temp;
+        });
+    },
+  },
 });
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -104,9 +125,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
@@ -125,13 +146,13 @@ export default defineComponent({
 
 #ion-label {
   font-size: small;
-  color:#8c8c8c;
+  color: #8c8c8c;
   padding-left: 20px;
 }
 
 #ingredientsList {
   padding-left: 55px;
   line-height: 0px;
-  color:#8c8c8c;
+  color: #8c8c8c;
 }
 </style>
